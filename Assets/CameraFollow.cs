b44086737117d49cd7cb97e5ec7eef54f5ad42f1
unknown <46870555+naomirror@users.noreply.github.com>
2021-03-21 19:38:06 +0000

    first commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;
+
+	public float smoothSpeed = 0.125f;
+
+	public Vector3 cameraOffset;
+
+	void FixedUpdate ()
+	{
+		//Vector3 desiredPosition = target.position + cameraOffset;
+		//Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+		//transform.position = smoothedPosition;
+		//transform.eulerAngles = target.eulerAngles;
+		//transform.rotation = target.transform.rotation;
+		//transform.RotateAround (target.transform.position, Vector3.up, target.rotation.y);
+		transform.LookAt (target);
+	}
+}
---
+++ Assets/CarMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+	public GameObject backLeftWheel, backRightWheel, frontLeftWheel, frontRightWheel;
+    WheelCollider blWheel, brWheel,flWheel,frWheel;
+	public Rigidbody car;
+	// Start is called before the first frame update
+	public float torque, brakeTorque, maxSpeed, highSpeedSteeringAngle, lowSpeedSteeringAngle;
+    void Start()
+    {
+		car.centerOfMass = new Vector3 (0.0f, -0.75f, .35f);
+		blWheel = backLeftWheel.GetComponent<WheelCollider> ();
+		brWheel = backRightWheel.GetComponent<WheelCollider> ();
+		flWheel = frontLeftWheel.GetComponent<WheelCollider>();
+		frWheel = frontRightWheel.GetComponent<WheelCollider> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float carVelocity = this.gameObject.GetComponent<Rigidbody> ().velocity.magnitude;
+		if (Input.GetAxis ("Vertical") > 0 && carVelocity < maxSpeed) {
+			blWheel.motorTorque = torque;
+			brWheel.motorTorque = torque;
+		} else if (Input.GetAxis ("Vertical") < 0 && carVelocity > 0) {
+			blWheel.brakeTorque = brakeTorque;
+			brWheel.brakeTorque = brakeTorque;
+		} 
+			/*NOT WORKING FOR REVERSING
+			 * else if (Input.GetAxis ("Vertical") < 0 && carVelocity <= 0){
+			blWheel.motorTorque = -torque;
+			brWheel.motorTorque = -torque;
+			}
+			*/
+		else
+		{
+			blWheel.brakeTorque = 0;
+			brWheel.brakeTorque = 0;
+			blWheel.motorTorque = 0;
+			brWheel.motorTorque = 0;
+		}
+		if (Input.GetAxis ("Horizontal") != 0) {
+			flWheel.steerAngle = lowSpeedSteeringAngle*Input.GetAxis ("Horizontal");
+			frWheel.steerAngle = lowSpeedSteeringAngle*Input.GetAxis ("Horizontal");
+		} 
+	}
+	void OnCollisionEnter(Collision collision){
+		Debug.Log ("hit");
+		if (collision.gameObject.name == "road") {
+			Debug.Log ("hit the road");
+		}
+	}
+}
