c882653d4c1ad3fd90cd4f9e20f4cfaf90d27b35
unknown <46870555+naomirror@users.noreply.github.com>
2021-03-21 23:05:43 +0000

    road class and updated prefabs

--- Assets/Road.cs
+++ Assets/Road.cs
@@ -4,58 +4,24 @@ using UnityEngine;
 
 public class Road : MonoBehaviour
 {
-	public Transform extraRoadBegin;
-	public Transform roadBegin;
-	public Transform road15Begin;
-	public Transform road45Begin;
-	public Transform roadUTurnBegin;
+	public Transform roadBeginning;
 	public Transform roadEnd;
-	public Transform road15End;
-	public Transform road45End;
-	public Transform roadUTurnEnd;
-	public GameObject road;
-	public Transform lastRoadEnd;
     // Start is called before the first frame update
     void Start()
     {
-		extraRoadBegin = GameObject.Find ("Road15Anchor (1)").GetComponent<Transform>();
-		roadBegin = this.transform;
-		road15Begin = GameObject.Find ("Road15Anchor").GetComponent<Transform> ();
-		road15End = road15Begin.transform.Find ("road15").Find("RoadEnd");
-		road45Begin = GameObject.Find ("Road45Anchor").GetComponent<Transform> ();
-		road45End = road45Begin.transform.Find ("road 45").Find("RoadEnd");
-		roadUTurnBegin = GameObject.Find ("RoadUTurnAnchor").GetComponent<Transform> ();
-		roadUTurnEnd = roadUTurnBegin.transform.Find ("road u turn").Find ("RoadEnd");
-		roadEnd = this.transform.Find ("road 30").Find("RoadEnd");
-		road = GameObject.Find ("road");
-		lastRoadEnd = road.GetComponent<Transform>().Find ("RoadEnd");
-
-	
+		roadBeginning = this.GetComponent<Transform> ();
+		roadEnd = roadBeginning.GetChild(0).Find("RoadEnd");
     }
 
     // Update is called once per frame
     void Update()
     {
-		roadBegin.position = lastRoadEnd.position;
-		roadBegin.rotation = lastRoadEnd.rotation;
-		road15Begin.position = roadEnd.position;
-		road15Begin.rotation = roadEnd.rotation;
-		road45Begin.position = road15End.position;
-		road45Begin.rotation = road15End.rotation;
-		roadUTurnBegin.position = road45End.position;
-		roadUTurnBegin.rotation = road45End.rotation;
-		extraRoadBegin.position = roadUTurnEnd.position;
-		extraRoadBegin.rotation = roadUTurnEnd.rotation;
-
-
-
-
-
-
-
-
-
 
 
     }
+
+	public void setPosition(Vector3 position, Quaternion rotation){
+		roadBeginning.position = position;
+		roadBeginning.rotation = rotation;
+	}
 }
