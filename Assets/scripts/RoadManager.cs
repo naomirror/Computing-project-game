397d296c225caac45aaef6830912150bc7b8c54a
unknown <46870555+naomirror@users.noreply.github.com>
2021-03-23 19:55:21 +0000

    working random generation without left hand pieces

---
+++ Assets/scripts/RoadManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour
+{
+	public List<GameObject> roads;
+	public Transform lastRoad;
+	public Transform newRoad;
+    // Start is called before the first frame update
+	IEnumerator Start()
+    {
+		while (true) {
+			newRoad = Instantiate (roads [Random.Range(0,6)], lastRoad.position, lastRoad.rotation 	* Quaternion.Euler(180f*Random.Range(0,2), 0f, 0f)).GetComponent<Transform> ();
+			lastRoad = newRoad.GetChild(0).Find("RoadEnd");
+			yield return new WaitForSeconds (3f);
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
