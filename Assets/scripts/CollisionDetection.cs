7353ce9e75fa1969fea6e94e95e371fccbdee422
Kyran <46870555+naomirror@users.noreply.github.com>
2021-03-24 18:02:08 +0000

    added collision detection (work in progress)

---
+++ Assets/scripts/CollisionDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void OnCollisionEnter(Collision col){
+
+		if (this.transform.parent.position != col.transform.Find ("RoadEnd").position && this.transform.Find("RoadEnd").position != col.transform.parent.position  &&this.transform.parent.rotation != col.transform.Find ("RoadEnd").rotation && this.transform.Find("RoadEnd").rotation != col.transform.parent.rotation) {
+			Debug.Log (col.gameObject.name);
+		}
+
+	}
+		
+}
--- Assets/scripts/RoadManager.cs
+++ Assets/scripts/RoadManager.cs
@@ -7,13 +7,14 @@ public class RoadManager : MonoBehaviour
 	public List<GameObject> roads;
 	public Transform lastRoad;
 	public Transform newRoad;
+	public float spawnDelay = 1f;
     // Start is called before the first frame update
 	IEnumerator Start()
     {
 		while (true) {
 			newRoad = Instantiate (roads [Random.Range(0,5)], lastRoad.position, lastRoad.rotation 	* Quaternion.Euler(180f*Random.Range(0,2), 0f, 0f)).GetComponent<Transform> ();
 			lastRoad = newRoad.GetChild(0).Find("RoadEnd");
-			yield return new WaitForSeconds (3f);
+			yield return new WaitForSeconds (spawnDelay);
 		}
     }
 
