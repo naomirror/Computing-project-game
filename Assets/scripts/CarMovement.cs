2306e4915d3da8b13c4691489775cfdde551cafd
Kyran <46870555+naomirror@users.noreply.github.com>
2021-03-29 22:44:08 +0100

    minor adjustments to car movement and prefabs

--- Assets/scripts/CarMovement.cs
+++ Assets/scripts/CarMovement.cs
@@ -9,6 +9,9 @@ public class CarMovement : MonoBehaviour
 	public Rigidbody car;
 	// Start is called before the first frame update
 	public float torque, brakeTorque, maxSpeed, highSpeedSteeringAngle, lowSpeedSteeringAngle;
+
+	public Vector3 resetPosition;
+	public Quaternion resetRotation;
     void Start()
     {
 		car.centerOfMass = new Vector3 (0.0f, -0.75f, .35f);
@@ -28,17 +31,15 @@ public class CarMovement : MonoBehaviour
 		} else if (Input.GetAxis ("Vertical") < 0 && carVelocity > 0) {
 			blWheel.brakeTorque = brakeTorque;
 			brWheel.brakeTorque = brakeTorque;
+			flWheel.brakeTorque = brakeTorque;
+			frWheel.brakeTorque = brakeTorque;
 		} 
-			/*NOT WORKING FOR REVERSING
-			 * else if (Input.GetAxis ("Vertical") < 0 && carVelocity <= 0){
-			blWheel.motorTorque = -torque;
-			brWheel.motorTorque = -torque;
-			}
-			*/
 		else
 		{
 			blWheel.brakeTorque = 0;
 			brWheel.brakeTorque = 0;
+			flWheel.brakeTorque = 0;
+			frWheel.brakeTorque = 0;
 			blWheel.motorTorque = 0;
 			brWheel.motorTorque = 0;
 		}
@@ -46,11 +47,28 @@ public class CarMovement : MonoBehaviour
 			flWheel.steerAngle = lowSpeedSteeringAngle*Input.GetAxis ("Horizontal");
 			frWheel.steerAngle = lowSpeedSteeringAngle*Input.GetAxis ("Horizontal");
 		} 
+
+		if (this.transform.position.y < -1f) {
+			resetCar ();
+		}
 	}
+
+	void resetCar(){
+		this.GetComponent<Rigidbody>().isKinematic = true;
+		this.transform.position = resetPosition;
+		this.transform.rotation = resetRotation;
+		this.GetComponent<Rigidbody>().isKinematic = false;
+	}
+
 	void OnCollisionEnter(Collision collision){
 		Debug.Log ("hit " + collision.transform.name);
-
 		collision.transform.gameObject.tag = "InContact";
+
+		//Doesn't account for left hand turns flipping the parent 180degrees so car lands upside down. 
+		resetPosition = collision.transform.parent.position;
+		resetPosition.y = resetPosition.y + 1f;
+		resetRotation = collision.transform.parent.rotation* Quaternion.Euler(0f,-90f,0f);
+
 	}
 
 	void OnCollisionExit(Collision collision){
