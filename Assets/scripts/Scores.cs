e87bf2885620ba3338fc335f3b7ef3cfe311e7e7
Kyran <46870555+naomirror@users.noreply.github.com>
2021-04-12 01:36:38 +0100

    added car speed and averages

--- Assets/scripts/Scores.cs
+++ Assets/scripts/Scores.cs
@@ -5,21 +5,39 @@ using UnityEngine.UI;
 
 public class Scores : MonoBehaviour
 {
-	public Text speed, collectibles;
+	public Text speedText, collectiblesText, averageSpeedText, timeText;
 	public float averageSpeed;
-	public int collected = 0;
+	public int collected = 0, minutes, seconds;
 	public CarMovement cm;
+	public float time;
     // Start is called before the first frame update
     void Start()
     {
+		time = Time.time;
 		cm = this.gameObject.GetComponent<CarMovement> ();
     }
 
+	int averageCount = 1;
+	float speed = 0;
     // Update is called once per frame
     void Update()
     {
-		speed.text = "Speed: " + Mathf.Round(cm.carVelocity);
-		collectibles.text = "Collectibles: " + collected;
+		time = Time.time;
+		minutes = Mathf.FloorToInt (time / 60);
+		seconds = Mathf.FloorToInt (time % 60);
+		Debug.Log (time);
+		if (seconds == 30 || seconds == 60) {
+			averageCount = 1;
+			speed = Mathf.Round (cm.carVelocity);
+		}
+		averageCount += 1;
+		speed += Mathf.Round (cm.carVelocity);
+		averageSpeed = Mathf.Round(speed / averageCount);
+
+		averageSpeedText.text = "Average Speed: " + averageSpeed;
+		speedText.text = "Speed: " + Mathf.Round(cm.carVelocity);
+		collectiblesText.text = "Collectibles per minute: " + collected;
+		timeText.text = "Time: " + minutes + " mins " + seconds + " seconds";
     }
 
 	void OnCollisionEnter(Collision col){
