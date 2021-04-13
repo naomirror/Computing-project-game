bae709673a6e487f191a2efa90e6a1dee48ae98b
Kyran <46870555+naomirror@users.noreply.github.com>
2021-04-13 07:31:46 +0100

    fixed.. I think

--- Assets/scripts/RoadManager.cs
+++ Assets/scripts/RoadManager.cs
@@ -16,14 +16,88 @@ public class RoadManager : MonoBehaviour
 	CollisionDetection roadCollisionScript;
 	Renderer r;
 	// Start is called before the first frame update
-	void Start()
+	IEnumerator Start()
 	{
 		score = GameObject.Find ("Tocus").GetComponent<Scores> ();
 		setRoadProbabilities (13);
 		addRoadSegmentsToSpawn ();
 		roadCollisionScript = lastRoad.parent.GetComponent<CollisionDetection>();
+		while (true) {
+			if (first == true && (score.seconds == 30 || score.seconds == 59)) {
+				setRoadProbabilities (score.averageSpeed);
+				addRoadSegmentsToSpawn ();
+				first = false;
+				Debug.Log ("Populating at:" + score.minutes + ":" + score.seconds);
+			}
+			if (score.seconds != 30 && score.seconds != 59) {
+				first = true;
+			}
+			if (roadToSpawnIncrement <= roadSpawn.Count - 1) {
+					newRoad = Instantiate (roadSpawn [roadToSpawnIncrement], lastRoad.position, lastRoad.rotation * Quaternion.Euler (180f * Random.Range (0, 2), 0f, 0f)).GetComponent<Transform> ();
+					lastRoad = newRoad.GetChild (0).Find ("RoadEnd");
+					roadToSpawnIncrement += 1;
+					Debug.Log ("InstantiatingRoad");
+					if (newRoad.GetChild (0).name == "road") {
+						r = newRoad.GetChild (0).GetChild (0).GetComponent<Renderer> ();
+					} else {
+						r = newRoad.GetChild (0).GetComponent<Renderer> ();
+					}
+					float randomX = Random.Range (r.bounds.min.x, r.bounds.max.x);
+					float randomZ = Random.Range (r.bounds.min.z, r.bounds.max.z);
 
-
+					RaycastHit hit;
+					if (Physics.Raycast (new Vector3 (randomX, r.bounds.max.y + 5f, randomZ), -Vector3.up, out hit)) {
+						GameObject collectibleSpawn = Instantiate (collectible, new Vector3 (randomX, r.bounds.max.y + 1f, randomZ), Quaternion.identity);
+						//collectibleSpawn.transform.parent = newRoad.GetChild (0).transform;
+						//GameObject treeSpawn =Instantiate (tree, new Vector3 (randomX, r.bounds.max.y, randomZ), Quaternion.Euler(-90f,0f,0f));
+						//treeSpawn.transform.parent = newRoad.GetChild (0).transform;
+					} 
+					Debug.Log ("Increment: " + roadToSpawnIncrement);
+					roadCollisionScript = newRoad.GetChild (0).GetComponent<CollisionDetection> ();
+				yield return new WaitForSeconds (0.02f);
+				if (roadCollisionScript.colliding == true) {
+					//newRoad.gameObject.active = false;
+					Debug.Log ("COLLISION DETECTED");
+					if (newRoad.GetChild (0).name == "road") {
+						newRoad.GetChild (0).GetChild (0).GetComponent<MeshRenderer> ().enabled = false;
+					} else {
+						newRoad.GetChild (0).gameObject.GetComponent<MeshRenderer> ().enabled = false;
+					}
+					while (roadCollisionScript.colliding == true) {
+						Debug.Log ("Waiting on collision to finish at" + score.minutes + ":" + score.seconds);
+						bool wait = false;
+						foreach (GameObject road in roadCollisionScript.intersectingRoad) {
+							if (road.tag != "FinishedContact") {
+								wait = true;
+							}
+						}
+						if (wait == false) {
+							Debug.Log ("Destroying roads");
+							foreach (GameObject road in roadCollisionScript.intersectingRoad) {
+								Destroy (road.transform.parent.gameObject);
+							}
+							roadCollisionScript.colliding = false;
+							if (newRoad.GetChild (0).name == "road") {
+								newRoad.GetChild (0).GetChild (0).GetComponent<MeshRenderer> ().enabled = true;
+							} else {
+								newRoad.GetChild (0).gameObject.GetComponent<MeshRenderer> ().enabled = true;
+							}
+						}
+						if (first == true && (score.seconds == 30 || score.seconds == 59)) {
+							setRoadProbabilities (score.averageSpeed);
+							addRoadSegmentsToSpawn ();
+							first = false;
+							Debug.Log ("Populating at:" + score.minutes + ":" + score.seconds);
+						}
+						if (score.seconds != 30 && score.seconds != 59) {
+							first = true;
+						}
+						yield return new WaitForSeconds (1f);
+					}
+				}
+			}
+			yield return new WaitForSeconds (0.5f);
+		} 
 
 		/*
 		int x = 5;
@@ -184,77 +258,11 @@ public class RoadManager : MonoBehaviour
 	}
 
 	IEnumerator wait(){
-		yield return new WaitForSeconds (0.02f);
+		yield return new WaitForSeconds (0.5f);
 	}
 	// Update is called once per frame
 	void Update()
 	{
-			if (first == true && (score.seconds == 30 || score.seconds == 59)) {
-				setRoadProbabilities (score.averageSpeed);
-				addRoadSegmentsToSpawn ();
-				first = false;
-				Debug.Log ("Populating at:" + score.minutes + ":" + score.seconds);
-			}
-			if (score.seconds != 30 && score.seconds != 59) {
-				first = true;
-			}
-			if (roadToSpawnIncrement <= roadSpawn.Count - 1 ) {
-				if (roadCollisionScript.colliding == false){
-					newRoad = Instantiate (roadSpawn [roadToSpawnIncrement], lastRoad.position, lastRoad.rotation * Quaternion.Euler (180f * Random.Range (0, 2), 0f, 0f)).GetComponent<Transform> ();
-					lastRoad = newRoad.GetChild (0).Find ("RoadEnd");
-					roadToSpawnIncrement += 1;
-					Debug.Log ("InstantiatingRoad");
-					if (newRoad.GetChild (0).name == "road") {
-						r = newRoad.GetChild (0).GetChild (0).GetComponent<Renderer> ();
-					} else {
-						r = newRoad.GetChild (0).GetComponent<Renderer> ();
-					}
-					float randomX = Random.Range (r.bounds.min.x, r.bounds.max.x);
-					float randomZ = Random.Range (r.bounds.min.z, r.bounds.max.z);
-
-					RaycastHit hit;
-					if (Physics.Raycast (new Vector3 (randomX, r.bounds.max.y + 5f, randomZ), -Vector3.up, out hit)) {
-						GameObject collectibleSpawn = Instantiate (collectible, new Vector3 (randomX, r.bounds.max.y + 1f, randomZ), Quaternion.identity);
-						//collectibleSpawn.transform.parent = newRoad.GetChild (0).transform;
-						//GameObject treeSpawn =Instantiate (tree, new Vector3 (randomX, r.bounds.max.y, randomZ), Quaternion.Euler(-90f,0f,0f));
-						//treeSpawn.transform.parent = newRoad.GetChild (0).transform;
-					} 
-					Debug.Log ("Increment: " + roadToSpawnIncrement);
-					roadCollisionScript = newRoad.GetChild (0).GetComponent<CollisionDetection> ();
-				}
-			StartCoroutine (wait ());
-				if (roadCollisionScript.colliding == true) {
-					//newRoad.gameObject.active = false;
-					Debug.Log ("COLLISION DETECTED");
-					if (newRoad.GetChild (0).name == "road") {
-						newRoad.GetChild (0).GetChild (0).GetComponent<MeshRenderer> ().enabled = false;
-					} else {
-						newRoad.GetChild (0).gameObject.GetComponent<MeshRenderer> ().enabled = false;
-					}
-					if (roadCollisionScript.colliding == true) {
-						Debug.Log ("Waiting on collision to finish at" + score.minutes + ":" + score.seconds);
-						bool wait = false;
-						foreach (GameObject road in roadCollisionScript.intersectingRoad) {
-							if (road.tag != "FinishedContact") {
-								wait = true;
-							}
-						}
-						if (wait == false) {
-							Debug.Log ("Destroying roads");
-							foreach (GameObject road in roadCollisionScript.intersectingRoad) {
-								Destroy (road.transform.parent.gameObject);
-							}
-							roadCollisionScript.colliding = false;
-							if (newRoad.GetChild (0).name == "road") {
-								newRoad.GetChild (0).GetChild (0).GetComponent<MeshRenderer> ().enabled = true;
-							} else {
-								newRoad.GetChild (0).gameObject.GetComponent<MeshRenderer> ().enabled = true;
-							}
-						}
-					}
-				}
-
-				//yield return new WaitForSeconds (spawnDelay);
-			} 
+			
 	}
 }
